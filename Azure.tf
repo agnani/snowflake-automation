bc7876e6c442674fd084896313ab107556e90c99
agnani <38104516+agnani@users.noreply.github.com>
2023-09-19 14:56:16 +0530

    Update Azure.tf

--- Azure.tf
+++ Azure.tf
@@ -1,6 +1,18 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      # version = "0.69.0"
+      version = "0.58.2"
+    }
+  }
+}
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id = var.subscription_id
