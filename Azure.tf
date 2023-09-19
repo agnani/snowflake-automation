28fc7decfd66036bca5aaf7b21c7249de5e76579
agnani <38104516+agnani@users.noreply.github.com>
2023-09-19 14:33:57 +0530

    Update Azure.tf

--- Azure.tf
+++ Azure.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "Azure_snowflake" {
   }
 
   provisioner "file" {
-    source      = "${path.module}/license.json"
+    source      = var.data-iq-license
     destination = "/tmp/license.json"
   }
 
